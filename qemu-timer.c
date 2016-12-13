438c29919d8b84af1bb6063f8ea771cc8364fe90
Max Filippov <jcmvbkbc@gmail.com>
2016-12-13 15:45:29 -0800

    qemu-timer: don't get stuck when -icount is used
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- qemu-timer.c
+++ qemu-timer.c
@@ -138,7 +138,7 @@ static void qemu_clock_init(QEMUClockType type)
 
 bool qemu_clock_use_for_deadline(QEMUClockType type)
 {
-    return !(use_icount && (type == QEMU_CLOCK_VIRTUAL));
+    return 1 || !(use_icount && (type == QEMU_CLOCK_VIRTUAL));
 }
 
 void qemu_clock_notify(QEMUClockType type)
